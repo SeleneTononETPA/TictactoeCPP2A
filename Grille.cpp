c5a1d200fe51cf5d4f492f88936359ce333d3c10
Selene Tonon <selene.t.etpa@gmail.com>
2021-03-17 10:10:04 +0100

    correction système de coordonnées

--- Grille.cpp
+++ Grille.cpp
@@ -7,13 +7,13 @@
     Grille::Grille() { resetContent(); }
 
     // renvoie le contenu de la case de coordonnées x,y dans [0;2]
-    char Grille::getContent(int x, int y){ return _cases[x-1+(y-1)*3]; }
+    char Grille::getContent(int x, int y){ return _cases[x+(y)*3]; }
         
 
         // ajoute un contenu dans une case de coordonnées x,y dans [0;2]
         // le contenu est un entier non nul
     void Grille::setContent(int x, int y, char nouveauContenu){
-        _cases[x-1+(y-1)*3] = nouveauContenu;
+        _cases[x+(y)*3] = nouveauContenu;
     }
         
     void Grille::affiche(){
