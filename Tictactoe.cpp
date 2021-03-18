6e4a5bac8b914fa916ff624cfc210792adf3600f
Selene Tonon <selene.t.etpa@gmail.com>
2021-03-18 15:39:18 +0100

    Update Tictactoe.cpp

--- Tictactoe.cpp
+++ Tictactoe.cpp
@@ -3,22 +3,58 @@
 
 #include "Grille.h"
 #include "Tictactoe.h"
+#include <iostream>
 
 
-    Tictactoe::Tictactoe(){}
+    Tictactoe::Tictactoe() : _symboleCourant('X') {}
 
-    void Tictactoe::afficheGrille(){}
+    void Tictactoe::afficheGrille(){
+        _grilleDeJeu.affiche();
+    }
 
-    void Tictactoe::ajouteSymbole(int x, int y){}
+    void Tictactoe::ajouteSymbole(int x, int y){
+        _grilleDeJeu.setContent(x,y,_symboleCourant);
+    }
 
-    bool Tictactoe::testeVictoireVerticale(){}
+    bool Tictactoe::testeVictoireVerticale(){
+        for(int i=0;i<2;i++){
+            if(_grilleDeJeu.getContent(0,i)==_grilleDeJeu.getContent(1,i)
+            && _grilleDeJeu.getContent(0,i)==_grilleDeJeu.getContent(2,i)
+            && _grilleDeJeu.getContent(0,i)!=' ')
+            {return true;}
+        }
+        return false;
+    }
 
-    bool Tictactoe::testeVictoireHorizontale(){}
+    bool Tictactoe::testeVictoireHorizontale(){
+        for(int i=0;i<2;i++){
+            if(_grilleDeJeu.getContent(i,0)==_grilleDeJeu.getContent(i,1)
+            && _grilleDeJeu.getContent(i,0)==_grilleDeJeu.getContent(i,2)
+            && _grilleDeJeu.getContent(i,0)!=' ')
+            {return true;}
+        }
+        return false;   
+    }
 
-    bool Tictactoe::testeVictoireDiagonale(){}
+    bool Tictactoe::testeVictoireDiagonale(){
+        if(_grilleDeJeu.getContent(0,0)==_grilleDeJeu.getContent(1,1)
+            && _grilleDeJeu.getContent(0,0)==_grilleDeJeu.getContent(2,2)
+            && _grilleDeJeu.getContent(0,0)!=' ')
+            {return true;}
+        if(_grilleDeJeu.getContent(2,0)==_grilleDeJeu.getContent(1,1)
+            && _grilleDeJeu.getContent(2,0)==_grilleDeJeu.getContent(0,2)
+            && _grilleDeJeu.getContent(2,0)!=' ')
+            {return true;}
+        return false;
+    }
 
-    bool Tictactoe::testeJeuNul(){}
+    bool Tictactoe::testeJeuNul(){
+        return _numeroTour==10;
+        }
 
-    void Tictactoe::finTour(){}
+    void Tictactoe::finTour(){
+        _symboleCourant=='X'?_symboleCourant='O':_symboleCourant='X';
+        _numeroTour++;
+   }
 
 #endif
